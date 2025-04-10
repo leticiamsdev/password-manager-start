ee865a87739b82478eea6ec9d40016f3625cf7f9
leticiamsdev <leticiamariana2004@gmail.com>
2025-04-10 20:32:55 -0300

    commit find_password with file json

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from tkinter import *
 from random import *
 import json
 import pyperclip
+from numpy.ma.extras import count_masked
+
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -49,6 +51,23 @@ def save_txt():
             entry_website.delete(0, END)
             entry_password.delete(0, END)
 
+def find_password():
+    website = entry_website.get()
+    try:
+        with open('data.json', 'r') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Oops", message="Data Not Found")
+    else:
+        if website in data:
+            password = data[website].get("password")
+            messagebox.showinfo(title=website, message=f"Password: {password}")
+        else:
+            messagebox.showinfo(title="Oops", message=f"{website} not found")
+
+
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 window = Tk()
 window.title("Password Manage")
@@ -63,8 +82,8 @@ label_website = Label( text= "Website:")
 label_website.grid(row = 1, column =0)
 
 # entry of datas
-entry_website = Entry(window, width=35)
-entry_website.grid(row=1, column= 1, columnspan = 2)
+entry_website = Entry(window, width=21)
+entry_website.grid(row=1, column= 1)
 entry_website.focus()
 label_email = Label(window, text="Email/Username:")
 label_email.grid(row=2, column=0, )
@@ -82,6 +101,11 @@ button_password = Button( text="Generate Password", command = generate_password)
 button_password.grid(row=3, column = 2)
 button_add = Button(text="Add", width=36, command = save_txt)
 button_add.grid(row=4, column = 1,columnspan = 2)
+
+
+button_search = Button(text="Search", width=13, command=find_password)
+button_search.grid(row=1, column=2)
+
 
 
 
